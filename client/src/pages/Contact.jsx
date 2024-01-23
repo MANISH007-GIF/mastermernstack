4438e15a8f3c7394f7ebcdfc944251071e433029
MANISH007 <manishdhawan0496@gmail.com>
2024-01-23 22:18:05 +0530

    add

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../store/auth";
+import { toast } from "react-toastify";
+
 
 const defaultContactFormData = {
   username: "",
@@ -52,10 +54,10 @@ export const Contact = () => {
         setContact(defaultContactFormData);
         const data = await response.json();
         console.log(data);
-        alert("Message send successfully");
+        toast.success("Message send successfully");
       }
     } catch (error) {
-      alert("Message not send");
+      toast.error("Message not send");
       console.log(error);
     }
   };
